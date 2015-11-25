542db8070b2af6575ecb60c887f87b1cbedc41af
shvoidlee <shvoidlee@gmail.com>
2015-11-25 10:40:19 +0800

    fix UTF-8 (Chinese) character save & search blocked issue

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/BaseHapiFhirDao.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/BaseHapiFhirDao.java
@@ -1198,14 +1198,15 @@ public abstract class BaseHapiFhirDao implements IDao {
 	protected static String normalizeString(String theString) {
 		char[] out = new char[theString.length()];
 		theString = Normalizer.normalize(theString, Normalizer.Form.NFD);
-		int j = 0;
+		/*int j = 0;
 		for (int i = 0, n = theString.length(); i < n; ++i) {
 			char c = theString.charAt(i);
 			if (c <= '\u007F') {
 				out[j++] = c;
 			}
 		}
-		return new String(out).toUpperCase();
+		return new String(out).toUpperCase();*/
+		return theString.toUpperCase();
 	}
 
 }
