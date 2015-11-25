d7500a0c45abef609ad07795db1232a97eec8fbc
shvoidlee <shvoidlee@gmail.com>
2015-11-25 10:34:02 +0800

    revise server support to create both DSTU1 & DSTU2 resource

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/BaseHapiFhirResourceDao.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/dao/BaseHapiFhirResourceDao.java
@@ -855,7 +855,7 @@ public abstract class BaseHapiFhirResourceDao<T extends IResource> extends BaseH
 	@Override
 	public DaoMethodOutcome create(T theResource, String theIfNoneExist, boolean thePerformIndexing) {
 		if (isNotBlank(theResource.getId().getIdPart())) {
-			if (getContext().getVersion().getVersion().equals(FhirVersionEnum.DSTU1)) {
+			if (getContext().getVersion().getVersion().equals(FhirVersionEnum.DSTU1) || getContext().getVersion().getVersion().equals(FhirVersionEnum.DSTU2)) {
 				if (theResource.getId().isIdPartValidLong()) {
 					throw new InvalidRequestException(getContext().getLocalizer().getMessage(BaseHapiFhirResourceDao.class, "failedToCreateWithClientAssignedNumericId", theResource.getId().getIdPart()));
 				}
